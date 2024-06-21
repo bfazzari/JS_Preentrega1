58ac7ccd5ff3863e9d754c40ebbcb9877efc29f4
Bruno <brunojf2000@gmail.com>
2024-06-20 22:46:07 -0300

    Entrega 2

--- resto_b.js
+++ resto_b.js
@@ -1,83 +1,99 @@
 // VARIABLES GLOBALES
-let tarjTuResto = "N";
-let totalFinal=0;
+let totalFinal = 0;
 
 // FUNCION para generar el total de la cena
 function total_parcial_cena(beb, com) {
     return (beb + com);
 }
 
-function tu_resto(pagoParcial){
-    totalFinal = pagoParcial- (pagoParcial * .25);
+function pago_efectivo(pagoParcial) {
+    totalFinal = pagoParcial - (pagoParcial * .10);
     return totalFinal;
 }
 
-function pago_efectivo(pagoParcial){
-    totalFinal = pagoParcial- (pagoParcial * .10);
-    return totalFinal;
+function filtrarPorMozo(objMesa) {
+    return objMesa.numMozo == numeroMozo;
 }
-
-
 
 //////////////////////////////////////////////////////////////////////////
 
-let mozo = prompt("Indique su número de mozo (1 o 2)");
+let listaMesas = [];
+let mozo = "";
+let valorBedida = 1000;
+let valorMenu = 5000;
 
+//CONSTRUCTOR NUEVA MESA
 
-while (mozo != 1 && mozo != 2) {
-    alert("No existe el mozo Nº " + mozo);
-    mozo = prompt("Indique su número correcto de mozo (1 o 2)");
-}
+class Mesa {
 
-if (mozo == 1) {
-    alert("Bienvenido Hernán");
-} else {
-    alert("Bienvenida Julieta");
-}
-
-let bebida = parseInt (prompt("Ingrese el importe gastado en Bebidas"));
-
-let comida = parseInt (prompt("Ingrese el importe gastado en Comida"));
-
-let totalParcial = total_parcial_cena(bebida, comida);
-
-// Interactua con el Mozo
-
-let cerrar = prompt("Quiere cobrar la mesa ? - Ingrese SI o NO");
-
-
-while (cerrar != "NO" && cerrar != "SI") {
-    alert(cerrar + "no es una respuesta fue válida");
-    cerrar = prompt("Quiere cerrar y cobrar la mesa ? - Ingrese SI o NO");
-}
-
-if (cerrar == "SI") {
-
-    alert("RECORDÁ OFRECER NUESTRAS PROMOCIONES DISPONIBLES");
-    alert("Efectivo -10% // TURESTO -15% adicinal !");
-
-    let formaDePago = prompt("Cómo paga? /// E: Ef / T: Deb o Cred");
-
-    if (formaDePago == "E") {
-
-        tarjTuResto = prompt("Tiene TuResto? - SI o NO");
-
-        if (tarjTuResto == "SI") {
-          alert("Total a pagar con descuento + TuResto es de $" + tu_resto(totalParcial));
-
-        } else {
-          alert("Total a pagar en Efectivo es de $" + pago_efectivo(totalParcial));
-        }
-
-      
-    } else {
-        totalFinal = totalParcial+ (totalParcial * .21);
-        alert("Solo aceptamos en un solo pago");
-        alert("Total a pagar: $" + totalFinal );
+    constructor(numMesa, numMozo, cantComensales, cantMenues) {
+        this.numMesa = numMesa;
+        this.numMozo = numMozo;
+        this.cantComensales = cantComensales;
+        this.cantMenues = cantMenues;
+        this.totalFinal = 0;
     }
-    
+    calcularTotal(cantMenues) {
+        this.totalFinal = cantMenues * valorMenu;
+    }
+    getMesasAbiertas() {
+        console.log("----------------------");
+        console.log("Mozo: ", this.numMozo);
+        console.log("Mesa N°: ", this.numMesa);
+        console.log("Total consumido: $", this.totalFinal);
+        console.log("----------------------");
+    }
 }
-else {
-    alert("El monto total SIN iva hasta este momento es de: $" + totalParcial);
-    alert("La mesa sigue abierta hasta que quiera cerrarla");
+//////////////////////////////////////////////////////////////////////////
+while (mozo != "CERRAR") {
+    console.log("ALASKA RESTO");
+    console.log("");
+    mozo = prompt("Indique número de mozo (1 o 2) o CERRAR para dejar de cargar");
+
+    if (mozo != "CERRAR") {
+
+        let numMozo = mozo;
+
+        if (numMozo=="1"){
+            alert("Hola Juan Carlos");
+        }else if(numMozo=="2"){
+            alert("Hola Julieta");
+        }
+        let numMesa = prompt("Ingrese Número de mesa");
+        let cantComensales = prompt("Ingrese la cant de comensales");
+        let cantMenues = prompt("Ingrese la cant de menues que van a consumir");
+        let montoParcialMesa = cantMenues * valorMenu;
+
+        // CREO UN NUEVO OBJETO MESA - CREO UNA MESA NUEVA
+
+        let objMesa = new Mesa(numMesa, numMozo, cantComensales, cantMenues);
+        objMesa.calcularTotal(cantMenues);
+        listaMesas.push(objMesa);
+        console.log("La mesa se agregó exitosamente");
+        alert("Nueva Mesa creada N°: " + objMesa.numMesa);
+    }
+    console.log(listaMesas);
 }
+//VER MESAS
+    let menuOpciones = prompt("1- Ver todas las mesas abiertas 2- Ver mesas asignadas por mozo");
+    let numeroMozo;
+    if (menuOpciones == "1") {
+        console.log("-------- Listado de mesas abiertas --------");
+        for (let mesa of listaMesas) {
+            mesa.getMesasAbiertas();
+        }
+    }
+    else if (menuOpciones == "2") {
+        numeroMozo = prompt("Ingresa el número de mozo por filtrar mesas");
+        let filtroMozoPorMesa = listaMesas.filter(filtrarPorMozo);
+        if (filtroMozoPorMesa.length != 0) {
+            console.log("--------Mesas por Mozo --------");
+            for (let mesa of filtroMozoPorMesa) {
+                mesa.getMesasAbiertas();
+            }
+            console.log("-------------------------------------------");
+        }
+        else {
+            console.log("No hay mesas asignadas al mozo ", numeroMozo);
+        }
+    }
